ccf507829f8e4f82780aae07cdb2208e4ede22c9
Michael Gutierrez <findmagpie@gmail.com>
2023-10-31 21:10:53 -0700

    snoogums astro-ph arxiv 2023

--- snoogums.py
+++ snoogums.py
@@ -1,30 +1,30 @@
 import yagmail
+import arxiv
 try:
     from auth import yagmail_auth
 except ImportError:
     yagmail_auth = "yagmail_auth.json"
-import pickle
-from datetime import datetime, timedelta
 
-time_from_start = datetime.now()-datetime(2022, 11, 25, 23, 22)
-n = int(time_from_start/timedelta(minutes=15))
-s = str((n+1)*4)
-s = "0"*max(0, 5-len(s))+s
-contents = [f"<img src='https://storage.googleapis.com/guutz/among-meme/among_{s}.png'>"]
+# Search for the latest paper in the "astro-ph" category (Astrophysics).
+papers = arxiv.query(query="cat:astro-ph", sort_by="submitted", sort_order="descending", max_results=1)
 
-def send(n,m):
+if papers:
+    latest_paper = papers[0]
+    title = latest_paper['title']
+    published_datetime = latest_paper['published']
+    link = latest_paper['pdf_url']
+    abstract = latest_paper['summary']
+    print("Title of the latest arXiv astrophysics paper:")
+    print(title)
     with yagmail.SMTP("magpie31415@gmail.com", oauth2_file=yagmail_auth) as yag:
         yag.send(
-            to="snoogums@googlegroups.com",
-            subject=f"C͓̽L͓̽U͓̽E͓̽L͓̽E͓̽S͓̽S͓̽ #{n}",
-            contents=m,
+            to="findmagpie@gmail.com",
+            subject=title,
+            contents=f"""Guys this is huge! Astrophysicists just discovered {title}!!!
+            
+            ---------------------------------------------------------------------------
+            {published_datetime}
+            {abstract}""",
         )
-
-try:
-    n_prev = pickle.load(open("snoogums.p", "rb"))
-except EOFError:
-    n_prev = 2
-for i in range(n_prev+1,n+1):
-    if int(s)>=4 and int(s)<=1296:
-        send(i, contents)
-pickle.dump(n, open("snoogums.p", "wb"))
+else:
+    print("No astrophysics papers found")
